1e9553de9a02f5f64e7da835012055bbc357862a
Tomas Weiss <tomasw@salsitasoft.com>
2018-03-06 12:32:55 +0100

    wip

--- .circleci/dockerize.sh
+++ .circleci/dockerize.sh
@@ -2,9 +2,8 @@
 
 BUILD="tomkis/centralhub:$CIRCLE_BUILD_NUM"
 
-cd ../
 docker build -t "$BUILD" .
 
 # USER AND PASS DEFINED IN CCI VARIABLES
-# docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
-# docker push "$BUILD"
+docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
+docker push "$BUILD"
