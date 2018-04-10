47169f3e13663c6ef6053156e0d3dd9bbde34def
Tomas Weiss <tomasw@salsitasoft.com>
2018-04-10 12:51:34 +0200

    test

--- .circleci/dockerize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BUILD="tomkis/testing-project:$CIRCLE_BUILD_NUM"
-
-docker build -t "$BUILD" .
-
-# USER AND PASS DEFINED IN CCI VARIABLES
-docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
-docker push "$BUILD"
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-
-const app = express();
-app.get("/", (req, res) => res.json({ works: true }));
-app.listen(process.env.PORT || 8080);
